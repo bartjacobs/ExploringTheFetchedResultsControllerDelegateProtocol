ed5a7c19e20f3cd940142fe90673b64fa4faed19
Bart Jacobs <developer@bartjacobs.com>
2017-03-08 09:49:02 +0100

    Create and implement NoteViewController

---
+++ Notes/NoteViewController.swift
@@ -0,0 +1,47 @@
+//
+//  NoteViewController.swift
+//  Notes
+//
+//  Created by Bart Jacobs on 08/03/2017.
+//  Copyright © 2017 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    // MARK: - Properties
+
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var contentTextView: UITextView!
+
+    // MARK: -
+
+    var note: Note?
+
+    // MARK: - View Life Cycle
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Update User Interface
+        titleTextField.text = note?.title
+        contentTextView.text = note?.content
+    }
+
+    // MARK: - Actions
+
+    @IBAction func save(_ sender: Any) {
+        guard let title = titleTextField.text else { return }
+        guard let content = contentTextView.text else { return }
+
+        // Update Note
+        note?.title = title
+        note?.content = content
+        note?.updatedAt = NSDate()
+
+        // Pop View Controller From Navigation Stack
+        _ = navigationController?.popViewController(animated: true)
+    }
+
+}
