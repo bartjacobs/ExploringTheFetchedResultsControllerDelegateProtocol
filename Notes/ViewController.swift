37485dbd3fcfbfaddf9414fb0728a270de29cba1
Bart Jacobs <developer@bartjacobs.com>
2017-03-08 08:28:49 +0100

    Populate table view with note records

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -13,12 +13,40 @@ class ViewController: UIViewController {
 
     // MARK: - Properties
 
+    @IBOutlet var tableView: UITableView!
+
+    // MARK: -
+
     fileprivate let coreDataManager = CoreDataManager(modelName: "Notes")
+
+    // MARK: -
+
+    fileprivate lazy var fetchedResultsController: NSFetchedResultsController<Note> = {
+        // Initialize Fetch Request
+        let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
+
+        // Add Sort Descriptors
+        let sortDescriptor = NSSortDescriptor(key: "createdAt", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+
+        // Initialize Fetched Results Controller
+        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: self.coreDataManager.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+
+        return fetchedResultsController
+    }()
 
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            let fetchError = error as NSError
+            print("Unable to Save Note")
+            print("\(fetchError), \(fetchError.localizedDescription)")
+        }
     }
 
     // MARK: - Navigation
@@ -31,6 +59,36 @@ class ViewController: UIViewController {
         // Configure View Controller
         viewController.delegate = self
     }
+
+}
+
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sections = fetchedResultsController.sections else {
+            return 0
+        }
+
+        let sectionInfo = sections[section]
+        return sectionInfo.numberOfObjects
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath)
+
+        // Fetch Note
+        let note = fetchedResultsController.object(at: indexPath)
+
+        // Configure Cell
+        cell.textLabel?.text = note.title
+        cell.detailTextLabel?.text = note.content
+
+        return cell
+    }
+
+}
+
+extension ViewController: UITableViewDelegate {
 
 }
 
